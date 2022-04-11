07cd50041b6fcbfdc4901106bfae64eb85b64dfd
Bekzat Karayev <bekzatkarayev@gmail.com>
2022-04-11 17:27:53 +0600

    Check connection to website

--- week3/ManagedThreadingTask2/ImageDownloaderApp/Program.cs
+++ week3/ManagedThreadingTask2/ImageDownloaderApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace ImageDownloaderApp
+{
+    public class Program
+    {
+
+        public const string WebsiteUrl = "https://www.google.com/";
+        public static void Main()
+        {
+            Console.WriteLine("Start");
+            CheckConnection();
+            Console.ReadKey();
+        }
+
+        public static async void CheckConnection()
+        {
+            HttpClient client = new HttpClient();
+            var checkingResponse = await client.GetAsync(WebsiteUrl);
+            Console.WriteLine(checkingResponse.StatusCode);
+
+        }
+    }
+}
