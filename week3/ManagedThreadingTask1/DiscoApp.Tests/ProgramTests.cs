9fd7e309f374ba27ac54034c99f657297291c3f3
Bekzat Karayev <bekzatkarayev@gmail.com>
2022-04-11 13:11:56 +0600

    Add more unit tests

--- week3/ManagedThreadingTask1/DiscoApp.Tests/ProgramTests.cs
+++ week3/ManagedThreadingTask1/DiscoApp.Tests/ProgramTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.IO;
 using Xunit;
@@ -40,7 +41,8 @@ namespace DiscoApp.Tests
         public void CloseDisco_PrintsCorrectMessage()
         {
             //Arrange
-            var expected = "Tracklist has ended\r\nDancers go home\r\nPress any key to close the disco\r\n";
+            var expected = "Tracklist has ended\r\nDancers go home\r\n" +
+                "Press any key to close the disco\r\n";
             var stringWriter = new StringWriter();
             Console.SetOut(stringWriter);
 
@@ -50,5 +52,73 @@ namespace DiscoApp.Tests
             //Assert
             Assert.Equal(expected, stringWriter.ToString());
         }
+
+        [Fact]
+        public void PlayTrack_PrintsCorrectMessage()
+        {
+            //Arrange
+            var expected = "Now DJ plays Esposito in LATINO style\r\n";
+            var track = new Track("Esposito", "Latino");
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //Act
+            Program.PlayTrack(track);
+
+            //Assert
+            Assert.Equal(expected, stringWriter.ToString());
+        }
+
+        [Fact]
+        public void Dance_PrintsCorrectMessage()
+        {
+            //Arrange
+            var expected = "Dancer is moving elbows\r\n";
+            var track = new Track("Track 69", "Hardbass");
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //Act
+            Thread.CurrentThread.Name = "Dancer";
+            Program.Dance(track);
+
+            //Assert
+            Assert.Equal(expected, stringWriter.ToString());
+        }
+
+        [Fact]
+        public void GenerateTrack_CreatesProperTrack()
+        {
+            //Arrange
+            var id = 21;
+            var trackRock = new Track("Track 21", "Rock");
+            var trackLatino = new Track("Track 21", "Latino");
+            var trackHardbass = new Track("Track 21", "Hardbass");
+
+            //Act
+            var track = Program.GenerateTrack(id);
+
+            //Assert
+            Assert.True(track.Name == trackRock.Name);
+            Assert.True(track.Style == trackRock.Style || track.Style == trackLatino.Style
+                || track.Style == trackHardbass.Style);
+        }
+
+        [Fact]
+        public void PlayMusic_HandlesEmptyTracklist()
+        {
+            //Arrange
+            var expected = "";
+            var tracklist = new List<Track>();
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //Act
+            Program.PlayMusic(tracklist);
+
+            //Assert
+            Assert.Equal(expected, stringWriter.ToString());
+        }
+
     }
 }
