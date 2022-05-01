f03d28dda32f3df98c498467cd511e2fb4ddc6ce
Bekzat Karayev <bekzatkarayev@gmail.com>
2022-05-01 20:51:22 +0600

    Add initial solution to problem

--- week0/AlgorithmsTask1/SingleNumberApp/Program.cs
+++ week0/AlgorithmsTask1/SingleNumberApp/Program.cs
@@ -6,11 +6,27 @@ namespace SingleNumberApp
     {
         public static void Main(string[] args)
         {
+            throw new NotImplementedException();
         }
 
         public static int GetNonRepeatedNumber(int[] nums)
-        { 
-            return nums.Length;
+        {
+            var uniqueNums = new List<int>();
+            uniqueNums.Add(nums[0]);
+
+            for (int index = 1; index < nums.Length; index++)
+            {
+                if (uniqueNums.Contains(nums[index]))
+                {
+                    uniqueNums.Remove(nums[index]);
+                }
+                else
+                {
+                    uniqueNums.Add(nums[index]);
+                }
+            }
+
+            return uniqueNums.First();
         }
     }
 }
