bb96f477de224537449e986cfb3c3ecf564e51c9
Bekzat Karayev <bekzatkarayev@gmail.com>
2022-05-02 12:56:37 +0600

    Add optimal solution

--- week0/AlgorithmsTask1/SingleNumberApp.Tests/ProgramTests.cs
+++ week0/AlgorithmsTask1/SingleNumberApp.Tests/ProgramTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using SingleNumberApp;
+using System.Collections;
 
 namespace SingleNumberApp.Tests
 {
@@ -9,13 +10,49 @@ namespace SingleNumberApp.Tests
         [InlineData(new int[] { 7 }, 7)]
         [InlineData(new int[] { 2, 2, 1 }, 1)]
         [InlineData(new int[] { 4, 1, 2, 1, 2 }, 4)]
-        [InlineData(new int[] { 8, -2, 8, 3, 3, -14, -1, -2, -1}, -14)]
-        public void GetNonRepeatedNumber_ArrayOfNumbers_ReturnsCorrectNumber(int[] nums, int expected)
+        [InlineData(new int[] { 8, -2, 8, 3, 3, -14, -1, -2, -1 }, -14)]
+        public void GetNonRepeatedNumber_GivenArrayOfNumbers_ReturnsCorrectNumber(int[] nums, int expected)
         {
             // Arrange
 
             // Act
             var actual = Program.GetNonRepeatedNumber(nums);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CreateHashTable_GivenArrayOfNumbers_GeneratesExpectedHashtable()
+        {
+            // Arrange 
+            var nums = new int[] { 4, 2, 0, 4, 0 };
+            var expectedTable = new Hashtable();
+            var generatedTable = new Hashtable();
+            expectedTable.Add(4, "2");
+            expectedTable.Add(2, "1");
+            expectedTable.Add(0, "2");
+
+            // Act
+            generatedTable = Program.CreateHashtable(nums);
+
+            // Assert
+            Assert.Equal(expectedTable, generatedTable);
+        }
+
+        [Fact]
+        public void LookupValueOne_GivenHashtable_ReturnsKeyThatHasValueOne()
+        {
+            // Arrange
+            int expected = 2;
+            int actual;
+            var table = new Hashtable();
+            table.Add(4, "2");
+            table.Add(2, "1");
+            table.Add(0, "2");
+
+            // Act
+            actual = Program.LookupValueOne(table);
 
             // Assert
             Assert.Equal(expected, actual);
--- week0/AlgorithmsTask1/SingleNumberApp/Program.cs
+++ week0/AlgorithmsTask1/SingleNumberApp/Program.cs
@@ -10,7 +10,28 @@ namespace SingleNumberApp
             throw new NotImplementedException();
         }
 
+        // The idea of solution is pretty simple
+        // A hashtable is generated from given array of integers,
+        // where hashtable keys are elements of tha array and
+        // values are number of occurrence of these elements inside the array
+        // e.g. Array [1, 5, 1] produces hashtable {[1, "2"], [5, "1"]}
+        // After that this hashtable is traversed, until a value "1" is found, 
+        // and then its key is returned as a final result.
+        // Which means that the element of the initial array that occurs in the array
+        // only once is fouund.
+
+        // ContainsKey method is performed in O(1) time, single iteration through array 
+        // and hashtable is done in O(n) time, 
+        // therefore this algorithm has linear time complexity
+        // and uses additional memory not more than size of the input array 
+
         public static int GetNonRepeatedNumber(int[] nums)
+        {
+            var table = CreateHashtable(nums);
+            return LookupValueOne(table);
+        }
+
+        public static Hashtable CreateHashtable(int[] nums)
         {
             var numsTable = new Hashtable();
             for (int index = 0; index < nums.Length; index++)
@@ -25,7 +46,12 @@ namespace SingleNumberApp
                 }
             }
 
-            foreach (DictionaryEntry pair in numsTable)
+            return numsTable;
+        }
+
+        public static int LookupValueOne(Hashtable table)
+        {
+            foreach (DictionaryEntry pair in table)
             {
                 if (pair.Value.ToString() == "1")
                 {
@@ -33,7 +59,7 @@ namespace SingleNumberApp
                 }
             }
 
-            return nums[0];
+            return 0;
         }
     }
 }
